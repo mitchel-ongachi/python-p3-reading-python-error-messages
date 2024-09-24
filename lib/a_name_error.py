55e597354a0d48df201c8343f21510af7378a471
mitchel buyengo <mitchelongachi63@gmail.com>
2024-09-24 18:26:46 +0300

    first commit

--- lib/a_name_error.py
+++ lib/a_name_error.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
-
+hello_world = "Hello, World!"  # Defines the variable
 print(hello_world)
--- lib/testing/lib_test.py
+++ lib/testing/lib_test.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# lib/a_name_error.py
+
+
 
 import runpy
 
